25cc774e9552f170749ee700478e93060ac6528b
Samuel Amoah <snamoah@gmail.com>
2014-10-02 16:49:46 +0000

    Worked on generating random names

--- public/js/controllers/randomStudentController.js
+++ public/js/controllers/randomStudentController.js
@@ -5,26 +5,31 @@ function randomStudentController($scope) {
 		{name: "Esther Olatunde", path: $scope.path +"prince.jpg"}, 
 		{name: "Babara Okoto", path: $scope.path +"prince.jpg"},
 		{name: "Priscilla Hazel", path: ""},
+		{name: "Prince Anim", path: $scope.path+"prince.jpg"},
 		{name: "David Evhade", path: ""},
 		{name: "Kennedy", path: ""},
 		{name: "Ike", path: ""},
 		{name: "Innocent", path: ""},
 		{name: "Philip Nunoo", path: ""},
-		{name: ""}];
+		{name: "Cassandra Sarfo", path: ""},
+		{name: "Derik Frimpong", path: ""},
+		{name: "Panin Tenkorang", path: ""},
+		{name: "Paul Damalie", path: ""},
+		{name: "Barnabas", path: ""},
+		{name: "Laud Bruce Tagoe", path: ""},
+		{name: "Laud Bentil", path: ""},
+		{name: "Kwame Asiedu", path: ""},
+		{name: "Kwame Yeboah", path: ""},
+		{name: "Jerry King", path: ""},
+		{name: "Kelechi Uduagu", path: ""},
+		{name: "Afi Kwao", path: ""},
+		{name: "Moses", path: ""}];
 
 	$scope.randomStudents = [" ", " ", " "];
 
 	$scope.generateStudent = function() {
 		$scope.randomStudents[0] = $scope.students[Math.floor(Math.random() * $scope.students.length)];
-
-		var student = "";
-		while(student == $scope.randomStudents[0]){
-			$scope.randomStudents[1] = $scope.students[Math.floor(Math.random() * $scope.students.length)];
-		}
-
-		student = "";
-		while(student == $scope.randomStudents[0] || student == $scope.randomStudents[1]) {
-			$scope.randomStudents[2] = $scope.students[Math.floor(Math.random() * $scope.students.length)];
-		}
+		$scope.randomStudents[1] = $scope.students[Math.floor(Math.random() * $scope.students.length)];
+		$scope.randomStudents[2] = $scope.students[Math.floor(Math.random() * $scope.students.length)];
 	};
 }
